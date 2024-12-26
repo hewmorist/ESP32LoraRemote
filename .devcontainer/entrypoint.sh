c49c305ef6cac15094d5c6a4727e9e0a4f0d9852
Peter <hewmorist@gmail.com>
2024-12-26 20:45:00 +0000

    changed order in entrypoint script

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# make it so mc exits in current dir
-
-
-source home/vscode/.bashrc
-
 # restart udev so for new rules
 # NOTE: Need to issue usbipd attach --wsl --<busid> from Windows powershell 
 
 sudo service udev restart
+
+
+# make it so mc exits in current dir
+source home/vscode/.bashrc
