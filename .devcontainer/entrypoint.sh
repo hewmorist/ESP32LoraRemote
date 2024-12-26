9e62c088da22c924e23f4d9dd8fe4beca708f5b7
Peter <hewmorist@gmail.com>
2024-12-26 20:08:16 +0000

    fix entrypoint script

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -2,8 +2,8 @@
 
 # make it so mc exits in current dir
 
-echo "alias mc='. /usr/lib/mc/mc-wrapper.sh'" >> ~/.bashrc
-source ~/.bashrc
+echo "alias mc='. /usr/lib/mc/mc-wrapper.sh'" >> home/vscode/.bashrc
+source home/vscode/.bashrc
 
 # restart udev so for new rules
 # NOTE: Need to issue usbipd attach --wsl --<busid> from Windows powershell 
