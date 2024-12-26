3944848d016eebc2903d3f96cd8aaf3c30a20779
Peter <hewmorist@gmail.com>
2024-12-26 17:27:19 +0000

    corrected .bashrc

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # make it so mc exits in current dir
 
-{ echo ""; echo "alias ll='ls -la'"; } >> ~/.bashrc
+{ echo ""; echo "alias mc='. /usr/lib/mc/mc-wrapper.sh && mc'"; } >> ~/.bashrc
 source ~/.bashrc
 
 # restart udev so for new rules
