5403aa927626500637b9a30dcc15bdfd78ce5138
Peter Hew <hewmorist@yahoo.com>
2024-12-26 09:44:54 -0800

    Update entrypoint.sh

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -2,8 +2,8 @@
 
 # make it so mc exits in current dir
 
-{ echo ""; echo "alias mc='. /usr/lib/mc/mc-wrapper.sh && mc'"; } >> ~/.bashrc
-source ~/.bashrc
+# { echo ""; echo "alias mc='. /usr/lib/mc/mc-wrapper.sh && mc'"; } >> ~/.bashrc
+# source ~/.bashrc
 
 # restart udev so for new rules
 # NOTE: Need to issue usbipd attach --wsl --<busid> from Windows powershell 
