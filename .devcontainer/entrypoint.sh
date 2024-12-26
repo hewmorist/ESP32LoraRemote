4e8e787ff98e08d5eed77f2320153c2630d6f525
Peter <hewmorist@gmail.com>
2024-12-26 16:25:57 +0000

    Added udev mods mc exit mods

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# make it so mc exits in current dir
+
+{ echo ""; echo "alias ll='ls -la'"; } >> ~/.bashrc
+source ~/.bashrc
+
+# restart udev so for new rules
+# NOTE: Need to issue usbipd attach --wsl --<busid> from Windows powershell 
+
 sudo service udev restart
